f0f5570f8a4cf19525a8b56362906ebe7f77e66a
Igleer <milaqiigli@gmail.com>
2019-03-04 15:25:58 +0100

    updated player movement  component.

--- 002_unity (1)/002_unity/Assets/MGEComponents/MGEPlayerMovement.cs
+++ 002_unity (1)/002_unity/Assets/MGEComponents/MGEPlayerMovement.cs
@@ -7,10 +7,16 @@ public class MGEPlayerMovement : MGEComponent
 {
     [Range(0,1.5f)]
     public float speed;
+
+    public float slowDownAmount = 0.5f;
+
+    public float speedUpAmount = 1.0f;
     public override XmlElement ParseSelf(XmlDocument pScene)
     {
         XmlElement component = pScene.CreateElement("PlayerMovement");
         component.SetAttribute("speed",speed.ToString());
+        component.SetAttribute("slowDownAmount",slowDownAmount.ToString());
+        component.SetAttribute("speedUpAmount",speedUpAmount.ToString());
         return component;
     }
 }
