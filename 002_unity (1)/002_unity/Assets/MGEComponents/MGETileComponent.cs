c155c013edca7437320561cadc2e5549bd2399af
Igleer <milaqiigli@gmail.com>
2019-03-04 15:14:02 +0100

    updated tile component.

--- 002_unity (1)/002_unity/Assets/MGEComponents/MGETileComponent.cs
+++ 002_unity (1)/002_unity/Assets/MGEComponents/MGETileComponent.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 
 public class MGETileComponent : MGEComponent
 {
+    public enum TileType
+    {
+        DEFAULT,
+        ICE,
+        LAVA
+    }
+
+    public TileType tileType = TileType.DEFAULT;
+
+    public bool isPaintable = true;
     public override XmlElement ParseSelf(XmlDocument pScene)
     {
          XmlElement component = pScene.CreateElement("TileComponent");
+         component.SetAttribute("TileType",tileType.ToString());
+         component.SetAttribute("isPaintable",isPaintable.ToString());
+
          return component;
     }
 }
