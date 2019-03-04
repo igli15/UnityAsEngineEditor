9f385729650a852e9520981b2809c9c700540774
Igleer <milaqiigli@gmail.com>
2019-03-04 15:48:28 +0100

    new player data values added.

--- 002_unity (1)/002_unity/Assets/MGEComponents/MGEPlayerData.cs
+++ 002_unity (1)/002_unity/Assets/MGEComponents/MGEPlayerData.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class MGEPlayerData : MGEComponent
 {
     public int playerNumber = 2;
+
+    public Transform respawnTransform;
     public override XmlElement ParseSelf(XmlDocument pScene)
     {
         XmlElement component = pScene.CreateElement("PlayerData");
-        component.SetAttribute("playerNumber",playerNumber.ToString());
+        component.SetAttribute("playerNumber",playerNumber.ToString()); 
+
+        Vector3 pos = respawnTransform.position;
+        pos.x *= -1;
+        component.SetAttribute("respawnPosition",pos.ToString()); 
         return component;
     }
 }
