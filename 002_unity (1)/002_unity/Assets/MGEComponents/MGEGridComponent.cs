ffdf330e253da63dab6391a06d4ccbf132b8171e
Igleer <milaqiigli@gmail.com>
2019-03-07 22:38:01 +0100

    parsed ballon positions in the grid.

--- 002_unity (1)/002_unity/Assets/MGEComponents/MGEGridComponent.cs
+++ 002_unity (1)/002_unity/Assets/MGEComponents/MGEGridComponent.cs
@@ -4,8 +4,11 @@ using System.Xml;
 using UnityEngine;
 
 [RequireComponent(typeof(GridComponent))]
+
+
 public class MGEGridComponent : MGEComponent
 {
+    public Transform[] ballonTiles;
     public override XmlElement ParseSelf(XmlDocument pScene)
     {
         XmlElement component = pScene.CreateElement("GridComponent");
@@ -15,6 +18,17 @@ public class MGEGridComponent : MGEComponent
         component.SetAttribute("gridSizeX",gridComponent.GetGridSizeX().ToString());
         component.SetAttribute("gridSizeY",gridComponent.GetGridSizeY().ToString());
         component.SetAttribute("tileRadius",gridComponent.GetTileRadius().ToString());
+
+         XmlElement ballonTransforms = pScene.CreateElement("BallonTilePositions");
+
+        component.AppendChild(ballonTransforms);
+
+        foreach(Transform t in ballonTiles)
+        {
+            XmlElement e = pScene.CreateElement("ballonTile");
+            e.SetAttribute("pos",t.position.ToString("N4"));
+            ballonTransforms.AppendChild(e);
+        }
 
         return component;
     }
