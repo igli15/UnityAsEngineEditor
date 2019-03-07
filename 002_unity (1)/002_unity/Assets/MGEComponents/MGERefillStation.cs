6746400325384cee85c897331e91f839f5026e28
henrik.stromme <henstr3@gmail.com>
2019-03-07 20:07:49 +0100

    Tweaking values

---
+++ 002_unity (1)/002_unity/Assets/MGEComponents/MGERefillStation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class MGERefillStation : MGEComponent
+{
+    public float inkGainRate=0.1f;
+    public override XmlElement ParseSelf(XmlDocument pScene)
+    {
+        XmlElement component = pScene.CreateElement("RefillStationComponent");
+        component.SetAttribute("inkGainRate", inkGainRate.ToString());
+        return component;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
