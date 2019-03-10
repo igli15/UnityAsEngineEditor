9ae5a5170224bcde583c7bb45a65692c69165da1
Igleer <milaqiigli@gmail.com>
2019-03-10 15:30:45 +0100

    created mge cannon  component and parsed data.

---
+++ 002_unity (1)/002_unity/Assets/MGEComponents/MGECannonComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class MGECannonComponent : MGEComponent
+{
+    private enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public int shootingRange = 4;
+
+    public override XmlElement ParseSelf(XmlDocument pScene)
+    {
+         XmlElement component = pScene.CreateElement("CannonComponent");
+         component.SetAttribute("shootingRange",shootingRange.ToString());
+
+        float dot =Vector3.Dot(transform.forward,Vector3.forward);
+        Direction dir;
+
+        if(dot > 0.7f) dir = Direction.UP;
+        else if(dot < -0.7f) dir = Direction.DOWN;
+        else
+        {
+            float hDot = Vector3.Dot(transform.forward,Vector3.right);
+            if(hDot > 0f) dir = Direction.RIGHT;
+            else dir = Direction.LEFT;
+        }
+    
+        component.SetAttribute("FacingDir",dir.ToString());
+
+         return component;
+    }
+}
