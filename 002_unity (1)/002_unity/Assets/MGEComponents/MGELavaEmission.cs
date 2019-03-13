1650cd93304b7c55706edaa2b04cf6253d697091
Igleer <milaqiigli@gmail.com>
2019-03-13 14:42:01 +0100

    new ice/lava emission components.

---
+++ 002_unity (1)/002_unity/Assets/MGEComponents/MGELavaEmission.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class MGELavaEmission : MGEComponent
+{
+   public override XmlElement ParseSelf(XmlDocument pScene)
+    {
+         XmlElement component = pScene.CreateElement("LavaEmission");
+         return component;
+    }
+}
